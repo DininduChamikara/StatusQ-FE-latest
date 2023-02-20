d593b347ed6af70cb94744ebc6f15ee914756d84
dinindu99 <peiristd-im18057@stu.kln.ac.lk>
2023-02-21 00:45:01 +0530

    fixed: campaign payement view modal

---
+++ src/components/modals/campaignPaymentView.jsx
@@ -0,0 +1,159 @@
+import { Close } from "@mui/icons-material";
+import {
+  Box,
+  Card,
+  Divider,
+  IconButton,
+  Modal,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+function CampaignPaymentView({
+  campaignPaymentViewOpened,
+  setCampaignPaymentViewOpened,
+}) {
+  return (
+    <Modal open={campaignPaymentViewOpened}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          height: "100vh",
+          alignItems: "center",
+        }}
+      >
+        <Card sx={{ width: { xs: "95vW", lg: "30vw" }, py: 1, px: 2 }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography color={"primary"} sx={{ fontSize:18, fontWeight: "bold" }}>
+              Campaign Payment View
+            </Typography>
+            <IconButton
+              onClick={() => {
+                setCampaignPaymentViewOpened(false);
+              }}
+            >
+              <Close />
+            </IconButton>
+          </Box>
+          <Divider />
+          <Box sx={{ my: 1 }}>
+            <Box
+              sx={{
+                my:0.5,
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box width={"40%"}>
+                <Typography color={"secondary"} pl={1} pt={1}>
+                  CampaignID
+                </Typography>
+              </Box>
+              <Box width={"60%"}>
+                <Typography pl={1} pt={1}>
+                  dsfjfkjvkvf5d35dfv5f
+                </Typography>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                my:0.5,
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box width={"40%"}>
+                <Typography color={"secondary"} pl={1} pt={1}>
+                  DateTime
+                </Typography>
+              </Box>
+              <Box width={"60%"}>
+                <Typography pl={1} pt={1}>
+                  2022-12-02 09:45:57
+                </Typography>
+              </Box>
+            </Box>
+
+            <Box
+              sx={{
+                my:0.5,
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box width={"40%"}>
+                <Typography color={"secondary"} pl={1} pt={1}>
+                  Total Amount *
+                </Typography>
+              </Box>
+              <Box width={"60%"}>
+                <Typography pl={1} pt={1}>
+                  Rs. 550.00
+                </Typography>
+              </Box>
+            </Box>
+
+            <Box
+              sx={{
+                my:0.5,
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box width={"40%"}>
+                <Typography color={"secondary"} pl={1} pt={1}>
+                  Cost of views
+                </Typography>
+              </Box>
+              <Box width={"60%"}>
+                <Typography pl={1} pt={1}>
+                  Rs. 500.00
+                </Typography>
+              </Box>
+            </Box>
+
+            <Box
+              sx={{
+                my:0.5,
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box width={"40%"}>
+                <Typography color={"secondary"} pl={1} pt={1}>
+                  System Fee
+                </Typography>
+              </Box>
+              <Box width={"60%"}>
+                <Typography pl={1} pt={1}>
+                  Rs. 50.00
+                </Typography>
+              </Box>
+            </Box>
+            
+          </Box>
+        </Card>
+      </Box>
+    </Modal>
+  );
+}
+
+export default CampaignPaymentView;
